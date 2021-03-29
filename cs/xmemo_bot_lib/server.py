7e94c8898bbdf204adc6fa70a6c41079f2033df8
Sujal Singh <email.sujalsingh@gmail.com>
2021-03-29 17:20:41 +0530

    ignoring exceptions now because I am lazy

--- cs/xmemo_bot_lib/server.py
+++ cs/xmemo_bot_lib/server.py
@@ -8,8 +8,11 @@ print(msg_list)
 last_offset = msg_list[-1]["update_id"] - 2
 
 while True:
-    last_offset += 1
-    msg = xmemo.get_last_msg(last_offset)
-    msg_txt = msg[-1]["message"]["text"]
-    chat_id = msg[-1]["message"]["chat"]["id"]
-    xmemo.handle_response(msg_txt, chat_id)
+    try:
+        last_offset += 1
+        msg = xmemo.get_last_msg(last_offset)
+        msg_txt = msg[-1]["message"]["text"]
+        chat_id = msg[-1]["message"]["chat"]["id"]
+        xmemo.handle_response(msg_txt, chat_id)
+    except Exception():
+        pass
