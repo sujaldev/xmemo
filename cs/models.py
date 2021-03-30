d2e519bde4b8b29c655e428400e015b2835c5f75
Sujal Singh <email.sujalsingh@gmail.com>
2021-03-30 15:34:32 +0530

    trying django database instead of json now

--- cs/models.py
+++ cs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Question(models.Model):
+    question = models.TextField()
+    date = models.CharField(max_length=17)
+    answer = models.TextField()
+
+    def __repr__(self):
+        return self.question
